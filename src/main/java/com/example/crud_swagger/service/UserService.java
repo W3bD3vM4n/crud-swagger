2a459590f3f083767f26749682dbc374768d6115
Hassan Rahman <hr-awesome@hotmail.com>
2024-12-17 23:28:24 -0500

    First commit

---
+++ src/main/java/com/example/crud_swagger/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.crud_swagger.service;
+
+import com.example.crud_swagger.model.User;
+import com.example.crud_swagger.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private IUserRepository iUserRepository;
+
+    public List<User> getAllUsers() {
+        return iUserRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return iUserRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return iUserRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        iUserRepository.deleteById(id);
+    }
+
+}
